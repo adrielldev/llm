894501e3681d649640df8cfcb90d49703a387644
Zeit / Adrieldev <adrieldev@outlook.com>
2026-08-23 15:10:15 -0300

    chap 2.4: using two special tokens: <unk> for unknown tokens and <endoftext> to mark when a text is over and its coming the next one

--- tokenizing_text.py
+++ tokenizing_text.py
@@ -1,6 +1,6 @@
 import re
 
-class SimpleTokenizerV1:
+class SimpleTokenizerV2:
     def __init__(self,vocab):
         self.str_to_int = vocab
         self.int_to_str = {i:s for s, i in vocab.items()}
@@ -8,8 +8,11 @@ class SimpleTokenizerV1:
     def encode(self,text):
         preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)',text)
         preprocessed = [item.strip() for item in preprocessed if item.strip()]
+        preprocessed = [item if item in self.str_to_int
+                        else "<|unk|>" for item in preprocessed]
         ids = [self.str_to_int[s] for s in preprocessed]
         return ids
+    
     def decode(self,ids):
         text = " ".join([self.int_to_str[i] for i in ids])
         text = re.sub(r'\s+([,.?!"()\'])', r'\1',text)
@@ -21,12 +24,14 @@ preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)',raw_text)
 preprocessed = [item.strip() for item in preprocessed if item.strip()]
 
 
-all_words = sorted(set(preprocessed))
-vocab_size = len(all_words)
+all_tokens = sorted(list(set(preprocessed)))
+all_tokens.extend(["<|endoftext|>","<|unk|>"])
+vocab = {token:integer for integer, token in enumerate(all_tokens)}
 
-vocab = {token:integer for integer, token in enumerate(all_words)}
+tokenizer = SimpleTokenizerV2(vocab)
 
-tokenizer = SimpleTokenizerV1(vocab)
-
-text = "Hello, do you like tea?"
-tokenizer.encode(text)
+text1 = "hello do you like tea?"
+text2 = "in the sunlit terraces of the palace."
+text = " <|endoftext|> ".join((text1,text2))
+print(text)
+print(tokenizer.decode(tokenizer.encode(text)))
