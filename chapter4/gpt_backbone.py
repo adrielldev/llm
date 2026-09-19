53eb6726b5e53a22d225cd21fcdfebb6d73f921e
Zeit / Adrieldev <adrieldev@outlook.com>
2026-09-19 07:42:41 -0300

    chap/4.2: layer  normalization

--- chapter4/gpt_backbone.py
+++ chapter4/gpt_backbone.py
@@ -51,6 +51,19 @@ class DummyLayerNorm(nn.Module):
     def forward(self,x):
         return x
 
+
+class LayerNorm(nn.Module):
+    def __init__(self,emb_dim):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+    def forward(self,x):
+        mean = x.mean(dim=-1,keepdim=True)
+        var = x.var(dim=-1,keepdim=True,unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
 tokenizer = tiktoken.get_encoding("gpt2")
 batch = []
 txt1 = "Every effort moves you"
@@ -60,9 +73,17 @@ batch.append(torch.tensor(tokenizer.encode(txt1)))
 batch.append(torch.tensor(tokenizer.encode(txt2)))
 batch = torch.stack(batch,dim=0)
 
-
-torch.manual_seed(123)
 model = DummyGPTModel(cfg)
 logits = model(batch)
-print("Output shape:",logits.shape)
-print(logits)
+
+
+torch.manual_seed(123)
+batch_example = torch.randn(2,5)
+ln = LayerNorm(emb_dim=5)
+out_ln = ln(batch_example)
+torch.set_printoptions(sci_mode=False)
+mean = out_ln.mean(dim=-1,keepdim=True)
+var = out_ln.var(dim=-1,unbiased=False,keepdim=True)
+print("Mean:\n",mean)
+print("Var:\n",var)
+
